27c621f058e83cbca3ed1a398fd27e05ffaeb889
Shen <shushen2013@gmail.com>
2021-06-09 00:33:30 -0400

    change pk to intger

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `Price` (
 	`id` INT NOT NULL AUTO_INCREMENT UNIQUE,
-	`Date` DATETIME NOT NULL,
+	`Date_id` INT NOT NULL,
 	`Area_Code` varchar(8) NOT NULL,
 	`Total_Condo_Sold_Number` INT NOT NULL,
 	`Avg_Price` FLOAT NOT NULL,
@@ -9,17 +9,18 @@ CREATE TABLE `Price` (
 );
 
 CREATE TABLE `Date` (
-	`Date` DATETIME NOT NULL,
+	`Date_id` INT NOT NULL AUTO_INCREMENT,
+	`Date` DATETIME NOT NULL UNIQUE,
 	`Inflation` FLOAT NOT NULL,
 	`GDP` FLOAT NOT NULL,
 	`Metal_Price` FLOAT NOT NULL,
 	`NASDAQ` FLOAT NOT NULL,
-	PRIMARY KEY (`Date`)
+	PRIMARY KEY (`Date_id`)
 );
 
 CREATE TABLE `Toronto` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`Date` DATETIME NOT NULL,
+	`Date_id` INT NOT NULL,
 	`Prime_Rate` FLOAT NOT NULL,
 	`Median_Age` FLOAT NOT NULL,
 	`Participation_Rate` FLOAT NOT NULL,
@@ -31,14 +32,14 @@ CREATE TABLE `Toronto` (
 
 CREATE TABLE `Ontario` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`Date` DATETIME NOT NULL,
+	`Date_id` INT NOT NULL,
 	`Immigrants_International` FLOAT NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `New` (
 	`id` INT NOT NULL AUTO_INCREMENT UNIQUE,
-	`Date` DATETIME NOT NULL,
+	`Date_id` INT NOT NULL,
 	`Units_Number_Planned` INT NOT NULL,
 	`Units_Number_Construction` INT NOT NULL,
 	PRIMARY KEY (`id`)
@@ -46,18 +47,18 @@ CREATE TABLE `New` (
 
 CREATE TABLE `All` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`Date` DATETIME NOT NULL,
+	`Date_id` INT NOT NULL,
 	`Condo_Sold_Number` INT NOT NULL,
 	`Avg_Price` FLOAT NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-ALTER TABLE `Price` ADD CONSTRAINT `Price_fk0` FOREIGN KEY (`Date`) REFERENCES `Date`(`Date`);
+ALTER TABLE `Price` ADD CONSTRAINT `Price_fk0` FOREIGN KEY (`Date_id`) REFERENCES `Date`(`Date_id`);
 
-ALTER TABLE `Toronto` ADD CONSTRAINT `Toronto_fk0` FOREIGN KEY (`Date`) REFERENCES `Date`(`Date`);
+ALTER TABLE `Toronto` ADD CONSTRAINT `Toronto_fk0` FOREIGN KEY (`Date_id`) REFERENCES `Date`(`Date_id`);
 
-ALTER TABLE `Ontario` ADD CONSTRAINT `Ontario_fk0` FOREIGN KEY (`Date`) REFERENCES `Date`(`Date`);
+ALTER TABLE `Ontario` ADD CONSTRAINT `Ontario_fk0` FOREIGN KEY (`Date_id`) REFERENCES `Date`(`Date_id`);
 
-ALTER TABLE `New` ADD CONSTRAINT `New_fk0` FOREIGN KEY (`Date`) REFERENCES `Date`(`Date`);
+ALTER TABLE `New` ADD CONSTRAINT `New_fk0` FOREIGN KEY (`Date_id`) REFERENCES `Date`(`Date_id`);
 
-ALTER TABLE `All` ADD CONSTRAINT `All_fk0` FOREIGN KEY (`Date`) REFERENCES `Date`(`Date`);
+ALTER TABLE `All` ADD CONSTRAINT `All_fk0` FOREIGN KEY (`Date_id`) REFERENCES `Date`(`Date_id`);
